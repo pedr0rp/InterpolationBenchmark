c637e402cb3d92539878d7d00f5065c1ed0eb2fc
pedrooorp <pedro.rodriguesp@gmail.com>
2018-03-31 16:34:49 -0300

    Implemented save on CVSFile

--- src/com/pedrooorp/CSVFile.java
+++ src/com/pedrooorp/CSVFile.java
@@ -1,10 +1,31 @@
 package com.pedrooorp;
 
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVFile<T> {
 
+    Point[] points;
+    String filename;
 
-    public CSVFile(T x, T y) {
+    public CSVFile(String filename, Point[] points) {
+        this.points = points;
+        this.filename = filename;
+    }
+
+    public void save() throws IOException {
+
+
+        List<String> lines = new ArrayList<>();
+        for(int i = 0; i < points.length; i++) lines.add(points[i].x + ";" + points[i].y);
+        Path file = Paths.get(System.getProperty("user.home") + "/Desktop/" + filename + ".csv");
+        Files.write(file, lines, Charset.forName("UTF-8"));
 
 
     }
--- src/com/pedrooorp/Main.java
+++ src/com/pedrooorp/Main.java
@@ -1,13 +1,13 @@
 package com.pedrooorp;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
     private static final BigDecimal MIN = new BigDecimal(0.0f);
-    private static final BigDecimal MAX= new BigDecimal(999999.0f);
+    private static final BigDecimal MAX= new BigDecimal(100.0f);
     private static final int SCALE = 4;
     private static final int DOMAIN_SIZE = 100;
 
@@ -15,24 +15,31 @@ public class Main {
     public static void main(String[] args) {
 
         Point<BigDecimal>[] xyBD = new Point[DOMAIN_SIZE];
+        Point<Long>[] xyL = new Point[DOMAIN_SIZE];
 
         for(int i = 0; i < xyBD.length; i++) {
-            xyBD[i] = new Point<>(generateRandomBigDecimal(SCALE,  MIN, MAX),generateRandomBigDecimal(SCALE, MIN, MAX));
-            System.out.println(xyBD[i]);
+            xyBD[i] = new Point<>(new BigDecimal(i), randomBigDecimal(SCALE, MIN, MAX));
+            xyL[i] = new Point<>(convert(xyBD[i].x), convert(xyBD[i].y));
         }
 
+        CSVFile fileBD = new CSVFile("BigDecimal", xyBD);
+        CSVFile fileL = new CSVFile("Long", xyL);
+
+        try {
+            fileBD.save();
+            fileL.save();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     static long convert(BigDecimal value) {
-        return  value.scaleByPowerOfTen(value.scale()).longValue();
+        return value.scaleByPowerOfTen(value.scale()).longValue();
     }
 
-    static BigDecimal generateRandomBigDecimal(int scale, BigDecimal min, BigDecimal max) {
+    static BigDecimal randomBigDecimal(int scale, BigDecimal min, BigDecimal max) {
         BigDecimal number = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
         return number.setScale(scale, RoundingMode.DOWN);
     }
-
-
-
 }
